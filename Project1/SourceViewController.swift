d245929cb81541aaf02b3acc322812e88970acff
Jason Wong <jawong71@gmail.com>
2018-09-29 18:28:49 -0700

    first commit

---
+++ Project1/SourceViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SourceViewController.swift
+//  Project1
+//
+//  Created by Jason Wong on 2018-08-18.
+//  Copyright © 2018 Jason Wong. All rights reserved.
+//
+
+import Cocoa
+
+class SourceViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
